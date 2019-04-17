1f03f7e76eb0ccd84adeef502e20287f45b5f93b
charles yu <charles_ymt@hotmail.com>
2019-04-17 19:34:45 +0800

    string-to-integer-atoi

--- reverse-integer/Solution.java
+++ reverse-integer/Solution.java
@@ -19,4 +19,11 @@ class Solution {
 
         return x;
     }
+    
+    public int reverse(int x) {
+        long res = 0;
+        for (; x != 0; x /= 10)
+            res = res * 10 + x % 10;
+        return res > Integer.MAX_VALUE || res < Integer.MIN_VALUE ? 0 : (int) res;
+    }
 }
