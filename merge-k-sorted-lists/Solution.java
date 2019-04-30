7ecee4114f78ade5b59173e6409048c76958f32e
charles yu <charles_ymt@hotmail.com>
2019-04-30 10:33:45 +0800

    merge-k-sorted-lists  swap-nodes-in-pairs

---
+++ merge-k-sorted-lists/Solution.java
@@ -0,0 +1,21 @@
+public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                if (o1.val < o2.val) return -1;
+                else if (o1.val == o2.val) return 0;
+                else return 1;
+            }
+        });
+        ListNode node = new ListNode(0), tmp = node;
+        for (ListNode l : lists) {
+            if (l != null) queue.add(l);
+        }
+        while (!queue.isEmpty()) {
+            tmp.next = queue.poll();
+            tmp = tmp.next;
+            if (tmp.next != null) queue.add(tmp.next);
+        }
+        return node.next;
+}
